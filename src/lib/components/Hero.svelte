446b3dc06871e24e20e3bb31a11d2e179c274ea6
Carson <92652800+carsonSgit@users.noreply.github.com>
2025-01-26 08:17:30 -0500

    style: Addition of navigation button

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -6,6 +6,14 @@
     <div class="hero-content">
       <h1 class="hero-title">Ecovest</h1>
       <p class="hero-tagline">{tagline}</p>
+      <div class="button-grid">
+        <button class="ecovest-button" on:click={() => window.location.href = '/analytics'}>
+          Ecovest <span class="arrow">{'>>'}</span>
+        </button>
+        <button class="github-button">
+          GitHub
+        </button>
+      </div>
     </div>
   </section>
   
@@ -18,6 +26,7 @@
       height: 100vh;
       padding-left: 2rem;
       padding-right: 2rem;
+      margin-top: -3rem;
       background-color: #f4f4f9;
     }
     .hero-content {
@@ -34,4 +43,44 @@
       font-size: 2rem;
       color: #666;
     }
+
+    
+  .ecovest-button {
+    background-color: #96D291;
+    color: #333333;
+    border: none;
+    border-radius: 12px;
+    padding: 10px 20px;
+    font-size: 1em;
+    display: flex;
+    gap: 8px;
+    cursor: pointer;
+    font-size: 1.2rem;
+  }
+
+  .arrow {
+    font-weight: 500;
+    margin-top: 1px;
+  }
+
+  .ecovest-button:hover {
+    background-color: #7EBA72;
+  }
+
+  .button-grid {
+    display: flex;
+    gap: 1rem;
+    margin-top: 1rem;
+  }
+
+  .github-button {
+    background-color: #a1989828;
+    color: #333333;
+    border: solid 2px #3333335f;
+    border-radius: 12px;
+    padding: 10px 32px;
+    font-size: 1em;
+    cursor: pointer;
+    font-size: 1.2rem;
+  }
   </style>
